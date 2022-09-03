eaa6a926ae9d6d0fb93108384c142408cb85d831
Ivan Bowen <bowenivan16@gmail.com>
2022-09-03 19:57:10 +0300

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap"
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center my-2 ubuntu">Rick & Morty <span className="text-primary">Wiki</span></h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+          <div className='row'>
+            <Cards />
+            <Cards />
+            <Cards />
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
