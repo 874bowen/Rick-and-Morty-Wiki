9d0498d8d94523112bcf0d08d52f7066646e9d98
Ivan Bowen <bowenivan16@gmail.com>
2022-09-06 20:22:39 +0300

    add the cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Card, CardImg, CardBody, CardImgOverlay, CardText, CardTitle } from 'reactstrap';
 
 const Cards = ({results}) => {
   let display;
+  console.log(results);
   if (results){
     display = results.map(result => {
+      let {name, image, status, location, species} = result;
       return(
-        <div key={result.id} className='col-4'>
-         { result.name}
+        <div key={result.id} className='col-4 mb-2 position-relative'>
+          <Card>
+            <CardImg src={image} className="imgs-fluid" alt={name}/>
+            <CardBody>
+                <CardTitle className='fs-4 fw-bold'>{name}</CardTitle>
+                <div className='fs-6'>Last location</div>
+                <CardText className='fs-5'>{location.name}</CardText>
+            </CardBody>
+        </Card>
+        <div className='position-absolute badge bg-danger'>{status}</div>
         </div>
       );
     });
