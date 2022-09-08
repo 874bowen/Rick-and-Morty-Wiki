9c812c8b5862f50f108d3222392116af5ee2556e
Ivan Bowen <bowenivan16@gmail.com>
2022-09-08 17:41:52 +0300

    finish the pagination part

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -7,11 +7,18 @@ const Pagination = ({ info, pageNumber, setPageNumber }) => {
           // because it is taking time add the ? 
           // if info exists then add pages
           <ReactPaginate 
-          className='pagination justify-content-center' nextLabel="Next" previousLabel="Prev"  
-          nextClassName="btn btn-primary"
-          previousClassName='btn btn-primary'
+          className='pagination justify-content-center gap-4 my-4' nextLabel="Next >" previousLabel="< Prev"  
+          nextLinkClassName="btn btn-primary"
+          previousLinkClassName='btn btn-primary'
+          pageClassName='page-item'
+          pageLinkClassName='page-link'
+          activeClassName='active'
+          forcePage={pageNumber === 1? 0: pageNumber-1}
+          onPageChange={(data) => {
+               setPageNumber(data.selected+1);
+          }}
           pageCount={info?.pages} />
      )
 }
 
-export default Pagination
+export default Pagination;
