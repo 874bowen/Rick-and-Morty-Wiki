872276a148cd2d5c2dd46162e9fde9e4c06cd613
Ivan Bowen <bowenivan16@gmail.com>
2022-09-08 16:26:03 +0300

    add search and its styles

---
+++ src/components/search/search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Input } from 'reactstrap';
+import styles from "./Search.module.scss";
+
+const Search = ({ setPageNumber, setSearch }) => {
+  return (
+    <form className='d-flex justify-content-center gap-4 mb-3'>
+          <Input 
+          onChange={(e)=>{
+               // for search to begin from page number 1               
+               setPageNumber(1);
+               setSearch(e.target.value);
+          }}
+          placeholder='Search for characters' type="text" className={styles.input}/>
+          <Button
+           onClick={(e) => {
+               e.preventDefault();
+           }}
+           outline color='primary' className={`${styles.btn} btn`}>Search</Button> 
+    </form>
+  )
+}
+
+export default Search
