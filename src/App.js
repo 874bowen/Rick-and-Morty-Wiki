41774626d94dde0df6733d71ee973cccee3e03fe
Ivan Bowen <bowenivan16@gmail.com>
2022-09-08 16:24:49 +0300

    add search component to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap"
 import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
 
 function App() {
   let [pageNumber, setPageNumber] = useState(1); 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  let [search, setSearch] = useState("");
   let [fetchedData, updateFetchedData] = useState({});
   // destructuring which is same as fetchedData.rasults
   let {info, results} = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
   useEffect(() => {
     (async function (){
       let data = await fetch(api).then((res) => res.json());
@@ -20,6 +24,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-center my-2 ubuntu">Rick & Morty <span className="text-primary">Wiki</span></h1>
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
       <div className="container">
         <div className="row">
           <div className="col-3">
@@ -32,7 +37,7 @@ function App() {
           </div>
         </div>
       </div>
-      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber} />
     </div>
   );
 }
