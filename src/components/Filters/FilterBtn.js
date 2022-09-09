10833ca9e74ceb1954c59f965241790dcf954e2f
Ivan Bowen <bowenivan16@gmail.com>
2022-09-09 17:48:03 +0300

    add styling to the buttons in accordion and take the input

--- src/components/Filters/FilterBtn.js
+++ src/components/Filters/FilterBtn.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
-const FilterBtn = ({ life, name, index }) => {
+const FilterBtn = ({ item, name, index, task, setPageNumber }) => {
   return (
     <div>
-     <div className="form-check">
-  <input className="form-check-input" type="radio" name="name" id={`${name}-${index}`} />
-  <label className="btn btn-outline-primary" htmlFor={`${name}-${index}`} >{life}</label>
-</div>
+      <style jsx>
+      {`
 
+      .inp:checked + label {
+        background-color: #0b5ed7;
+        color: white;
+      }
+
+      input[type='radio']{
+        display: none;
+      }
+      `}
+      </style>
+      <div className="form-check">
+        <input
+         onClick={()=>{
+          setPageNumber(1);
+          task(item);
+         }}
+         className="form-check-input inp" type="radio" name={name} id={`${name}-${index}`} />
+        <label className="btn btn-outline-primary" htmlFor={`${name}-${index}`} >{item}</label>
+      </div>
     </div>
   )
 }
