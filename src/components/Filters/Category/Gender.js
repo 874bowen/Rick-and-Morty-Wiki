a715c871cd73d60a4e5b673b513d87c40ec3105c
Ivan Bowen <bowenivan16@gmail.com>
2022-09-09 17:47:09 +0300

    generate genders buttons

--- src/components/Filters/Category/Gender.js
+++ src/components/Filters/Category/Gender.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import FilterBtn from '../FilterBtn';
 
-const Gender = () => {
+const Gender = ({setPageNumber, setGender}) => {
+  let genders = ["female", "male", "genderless", "unknown"];
   return (
      <div className="accordion-item">
      <h2 className="accordion-header" id="headingOne">
-       <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+       <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
          Gender
        </button>
      </h2>
-     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-       <div className="accordion-body">
+     <div id="collapseOne" className="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+       <div className="accordion-body d-flex flex-wrap gap-3">
+        {genders.map((item, index) => {
+          return(
+            <FilterBtn key={index} task={setGender} setPageNumber={setPageNumber} name="gender" index={index} item={item} />
+          );
+        })}
        </div>
      </div>
    </div>
