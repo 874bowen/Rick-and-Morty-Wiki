169eb508cd3ff00656353e20eceebf26b35f813e
Ivan Bowen <bowenivan16@gmail.com>
2022-09-09 17:47:41 +0300

    generate Status buttons

--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import FilterBtn from '../FilterBtn'
 
-const Status = () => {
+const Status = ( {setStatus, setPageNumber}) => {
      let status = ["Alive", "Dead", "Unknown"]
   return (
      <div className="accordion-item">
      <h2 className="accordion-header" id="headingThree">
-       <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+       <button className="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
          Status
        </button>
      </h2>
-     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-       <div className="accordion-body">
-          {status.map((life, index) => (<FilterBtn key={index} name="status" index={index} 
-          life={life} />))}
+     <div id="collapseThree" className="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+       <div className="accordion-body  d-flex flex-wrap gap-3">
+          {status.map((item, index) => (<FilterBtn 
+          task={setStatus}
+          key={index} 
+          setPageNumber={setPageNumber}
+          name="status" 
+          index={index} 
+          item={item} />))}
        </div>
      </div>
    </div>
