c636e3cfaec8870cad4718f3adad3b39a2b5d95d
Ivan Bowen <bowenivan16@gmail.com>
2022-09-09 17:46:02 +0300

    destructure setStatus, setGender and setStatus passed from app.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,18 +1,26 @@
+import { clear } from '@testing-library/user-event/dist/clear'
 import React from 'react'
 import Gender from './Category/Gender'
 import Species from './Category/Species'
 import Status from './Category/Status'
 
-const Filters = () => {
+const Filters = ( {setSpecies, setGender, setStatus, setPageNumber} ) => {
+  clear = () => {
+    setSpecies("");
+    setGender("");
+    setStatus("");
+    setPageNumber(1);
+    window.location.reload(false);
+  }
   return (
     <div className='col-3'>
       <div className="text-center fw-bold fs-4 mb-4">Filter</div>
-      <div style={{cursor: "pointer"}} className="text-center mb-4 text-primary text-decoration-underline">Clear Filters</div>
+      <div onClick={clear} style={{cursor: "pointer"}} className="text-center mb-4 text-primary text-decoration-underline">Clear Filters</div>
 
       <div className="accordion" id="accordionExample">
-        <Species />
-        <Status />
-        <Gender />
+        <Status setStatus={setStatus} setPageNumber={setPageNumber} />
+        <Species setSpecies={setSpecies} setPageNumber={setPageNumber} />
+        <Gender setGender={setGender} setPageNumber={setPageNumber} />
 </div>
     </div>
   )
