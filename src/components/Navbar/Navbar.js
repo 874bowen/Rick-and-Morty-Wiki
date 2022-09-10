a420560fede66f734c4e4db06c6e0caa0a079760
Ivan Bowen <bowenivan16@gmail.com>
2022-09-10 21:22:59 +0300

    add styles to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import '../../App.css'
 
 
 // navlink is imported because it has a propery called activeClassName to design the active page
 const Navbar = () => {
   return (
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+     <nav className="navbar navbar-expand-lg mb-4 navbar-light bg-light">
      <div className='container'>
      <Link to="/" className="navbar-brand fs-2 fw-bold ubuntu">Rick & Morty <span className="text-primary">Wiki</span></Link>
      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-       <span className="navbar-toggler-icon"></span>
+        <style jsx>
+          {`
+          button[aria-expanded="false"] > .close {
+            dispay: none;
+          }
+          button[aria-expanded="true"] > .open {
+            dispay: none;
+          }
+          `}
+        </style>
+       <i className='fas fa-bars fw-bold text-dark open'></i>
+       <i className='fas fa-times fw-bold text-dark close'></i>
      </button>
      <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
        <div className="navbar-nav fs-5">
-         <NavLink to='/' className="nav-link active" >Characters</NavLink>
+         <NavLink to='/' activeClassName="active" className="nav-link" >Characters</NavLink>
          <NavLink to='/episodes' className=" nav-link" >Episodes</NavLink>
          <NavLink to='/locations' className="nav-link" >Locations</NavLink>
        </div>
