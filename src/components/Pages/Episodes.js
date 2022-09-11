d9fa17665e41e5d3a3d70035b24c1a75873ce3a9
Ivan Bowen <bowenivan16@gmail.com>
2022-09-11 18:17:20 +0300

    add each episode's character

--- src/components/Pages/Episodes.js
+++ src/components/Pages/Episodes.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cards from '../Cards/Cards';
+import InputGroup from '../Filters/Category/InputGroup';
 
 const Episodes = () => {
-  return (
-    <div>Episodes</div>
-  );
+     let [id, setId] = useState(1);
+     let [info, setInfo] = useState([]);
+     let api = `https://rickandmortyapi.com/api/episode/${id}`;
+     let {air_date, name} = info;
+     let [results, setResults] = useState([]);
+
+     useEffect(() => {
+          (async function(){
+               let data = await fetch(api).then((res) => res.json());
+               setInfo(data);
+
+               let allCharImgs = await Promise.all(
+                    data.characters.map((char) => {
+                         return fetch(char).then((res) => res.json());
+                    })
+               )
+               setResults(allCharImgs);
+          })();
+     }, [api])
+     return (
+          <div className="container">
+               <div className="row mb-4">
+                    <h1 className="text-center mb-4">
+                         Episode : <span className="text-primary">{name === ""? "Unknown" : name}</span>
+                    </h1>
+                    <h5 className="text-center">
+                    Air Date {air_date === ""?  "Unknown" : air_date}
+                    </h5>
+               </div>
+               <div className="row">
+                    <div className="col-3">
+                         <h4 className="text-center mb-4">
+                              Pick Episodes 
+                         </h4>
+                         <InputGroup setId ={setId} name="Episode" total={51} />
+                    </div>
+                    <div className="col-8">
+                         <div className="row">
+                              <Cards results={results} />
+                         </div>
+                    </div>
+               </div>
+          </div>
+     );
 }
 
 export default Episodes;
